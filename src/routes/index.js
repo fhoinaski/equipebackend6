d0cc9df8daf62b46abb26dac0f85ec4f794b5f27
ricardo-werner <wernerdive@gmail.com>
2023-09-13 14:04:45 -0300

    chore: descomentado o indes.js do routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const { Router } = require('express');
-// const { routesFromUsuario } = require('./usuario.routes');
-// const { routesFromUnidade } = require('./unidade.routes');
-// //const { routesFromLogin } = require('./login.routes');
-// const { routesFromGeracao } = require('./geracao.routes');
+const { routesFromUsuario } = require('./usuario.routes');
+const { routesFromUnidade } = require('./unidade.routes');
+//const { routesFromLogin } = require('./login.routes');
+const { routesFromGeracao } = require('./geracao.routes');
 
 const routes = Router();
 
 routes.use('/api', [
-    // routesFromUsuario(),
-    // routesFromUnidade(),
-    // //routesFromLogin(),
-    // routesFromGeracao(),
+    routesFromUsuario(),
+    routesFromUnidade(),
+    //routesFromLogin(),
+    routesFromGeracao(),
 ]);
 
 module.exports = routes;
