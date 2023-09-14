db66425a6f1a1a051610c4088dbb3301b5a80a18
fhoinaski <sukinodoncai@gmail.com>
2023-09-14 10:53:27 -0300

    correção dos metodos controller das rotas

--- src/routes/unidade.routes.js
+++ src/routes/unidade.routes.js
@@ -1,16 +1,12 @@
 const {
     createOneUnidade,
-    // listAllUnidades,
-    // listOneUnidade,
-    // listAllUnidadesAtivas,
-    // listAllUnidadesInativas,
-    // updateOneUnidade,
-    // updateOneLocal,
-    // updateOneMarca,
-    // updateOneModelo,
-    // updateOneStatus,
-    // deleteOneUnidade,
-    // restoreOneUnidade,
+    listAllUnidades,
+    listOneUnidade,
+    listAllUnidadesAtivas,
+    listAllUnidadesInativas,
+    updateOneUnidade,
+    deleteOneUnidade,
+    restoreOneUnidade,
 } = require('../controllers/unidades.controller');
 
 const { Router } = require('express');
@@ -21,17 +17,13 @@ class UnidadeRoutes {
     routesFromUnidade() {
         const unidadeRoutes = Router()
         unidadeRoutes.post('/createOneUnidade',  createOneUnidade);
-        // unidadeRoutes.get('/listAllUnidades', auth, listAllUnidades);
-        // unidadeRoutes.get('/listOneUnidade/:id', auth, listOneUnidade);
-        // unidadeRoutes.get('/listAllUnidadesAtivas', auth, listAllUnidadesAtivas);
-        // unidadeRoutes.get('/listAllUnidadesInativas', auth, listAllUnidadesInativas);
-        // unidadeRoutes.patch('/updateOneUnidade/:id', auth, updateOneUnidade);
-        // unidadeRoutes.patch('/updateOneLocal/:id', auth, updateOneLocal);
-        // unidadeRoutes.patch('/updateOneMarca/:id', auth, updateOneMarca);
-        // unidadeRoutes.patch('/updateOneModelo/:id', auth, updateOneModelo);
-        // unidadeRoutes.patch('/updateOneStatus/:id', auth, updateOneStatus);
-        // unidadeRoutes.delete('/deleteOneUnidade/:id', auth, deleteOneUnidade);
-        // unidadeRoutes.patch('/restoreOneUnidade/:id', auth, restoreOneUnidade);
+        unidadeRoutes.get('/listAllUnidades', listAllUnidades);
+        unidadeRoutes.get('/listOneUnidade/:id', listOneUnidade);
+        unidadeRoutes.get('/listAllUnidadesAtivas', listAllUnidadesAtivas);
+        unidadeRoutes.get('/listAllUnidadesInativas', listAllUnidadesInativas);
+        unidadeRoutes.put('/updateOneUnidade/:id', updateOneUnidade);
+        unidadeRoutes.delete('/deleteOneUnidade/:id',  deleteOneUnidade);
+        unidadeRoutes.patch('/restoreOneUnidade/:id', restoreOneUnidade);
         return unidadeRoutes;
     }
 }
